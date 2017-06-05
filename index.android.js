1d8248563e8901409d71d5f9d79863478bea62be
zhangyahong <354728907@qq.com>
2017-06-05 10:10:01 +0800

    git test how to commit

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ export default class AwesomeProject extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React !
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
